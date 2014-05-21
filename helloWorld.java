510e4ad085ba81a0ec5d9225ccc4a77857501d74
praneeth <pvallem@tetrationanalytics.com>
2014-05-21 14:50:19 -0700

    commit helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,9 +2,13 @@
 
 public class helloWorld {
 
+public static void print(String s){
+System.out.println(s);
+}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		print("Hello world this is first program");
 
 	}
 
