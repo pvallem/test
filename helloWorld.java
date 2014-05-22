372a342efd9c6d4ba40e7680d94f4986e41a5ac1
unknown <pvallem@pvallem-WS.cisco.com>
2014-05-21 18:48:11 -0700

    helloWorld program changed

--- helloWorld.java
+++ helloWorld.java
@@ -6,10 +6,21 @@ public static void print(String s){
 System.out.println(s);
 }
 
+public helloWorld(){
+name = new String();
+ this.name="default";
+ }
+ 
+public helloWorld(String nm){
+ name = new String();
+ this.name=nm;
+ }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		print("Hello world this is first program");
+		helloWorld hW = new helloWorld("Hi created a hello world program");
+		print(hW.name);
 
 	}
 
+	private static String name;
 }
