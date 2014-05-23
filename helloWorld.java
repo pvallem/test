759015d6a508fe002aa5544090274f011ddac44a
unknown <pvallem@pvallem-WS.cisco.com>
2014-05-23 10:03:54 -0700

    small chnage to helloWorld Program

--- helloWorld.java
+++ helloWorld.java
@@ -2,22 +2,26 @@
 
 public class helloWorld {
 
-public static void print(String s){
-System.out.println(s);
-}
+	public static void print(String s){
+		System.out.println(s);
+	}
 
-public helloWorld(){
-name = new String();
- this.name="default";
- }
+	public helloWorld(){
+		name = new String();
+ 		this.name="default";
+ 	}
  
-public helloWorld(String nm){
- name = new String();
- this.name=nm;
- }
+	public helloWorld(String nm){
+ 		name = new String();
+ 		this.name=nm;
+ 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		helloWorld hW = new helloWorld("Hi created a hello world program");
+		print(hW.name);
+
+		helloWorld hW1 = new helloWorld();
 		print(hW.name);
 
 	}
